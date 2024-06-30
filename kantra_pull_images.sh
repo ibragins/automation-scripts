7449ed6169bb2899b86c46c3de4ddaf1c6c16b09
Igor Braginsky <ibragins@redhat.com>
2024-06-30 16:35:38 +0300

    Added tool to pull kantra images to the local host
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

---
+++ kantra_pull_images.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+version="7.1.0"
+declare -A images
+declare -A hashes
+
+if [ "$1" == "docker" ]; then
+    CMD="docker"
+else
+    CMD="podman"
+fi
+
+echo "Attention, using $CMD as container engine"
+echo "Pulling images..."
+for item in java generic dotnet; do
+    package=$(cat output.txt | grep registry | grep "$item" | cut -d "\"" -f 2|cut -d "/" -f 3)
+    docker pull "registry-proxy.engineering.redhat.com/rh-osbs/mta-$package"
+done
+
+echo --------------------
+echo "Getting hash..."
+for item in java generic dotnet; do
+    hashes[$item]=$(docker images | grep "$item" | grep none | awk '{print $3}')
+    echo "$item's hash is ${hashes[$item]}"
+done
+
+echo --------------------
+echo "Tagging images..."
+for item in java generic dotnet; do
+    docker tag ${hashes[$item]} "registry.redhat.io/mta/mta-$item-external-provider-rhel9:$version"
+    echo "$item image is tagged"
+done
