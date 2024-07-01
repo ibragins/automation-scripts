9588d70e6caca2f3327b1f2a42cc1975fb1512f7
Igor Braginsky <ibragins@redhat.com>
2024-07-01 13:41:00 +0300

    Fix for kantra pull images script
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- kantra_pull_images.sh
+++ kantra_pull_images.sh
@@ -13,19 +13,19 @@ echo "Attention, using $CMD as container engine"
 echo "Pulling images..."
 for item in java generic dotnet; do
     package=$(cat output.txt | grep registry | grep "$item" | cut -d "\"" -f 2|cut -d "/" -f 3)
-    docker pull "registry-proxy.engineering.redhat.com/rh-osbs/mta-$package"
+    $CMD pull "registry-proxy.engineering.redhat.com/rh-osbs/mta-$package"
 done
 
 echo --------------------
 echo "Getting hash..."
 for item in java generic dotnet; do
-    hashes[$item]=$(docker images | grep "$item" | grep none | awk '{print $3}')
+    hashes[$item]=$($CMD images | grep "$item" | grep none | awk '{print $3}')
     echo "$item's hash is ${hashes[$item]}"
 done
 
 echo --------------------
 echo "Tagging images..."
 for item in java generic dotnet; do
-    docker tag ${hashes[$item]} "registry.redhat.io/mta/mta-$item-external-provider-rhel9:$version"
+    $CMD tag ${hashes[$item]} "registry.redhat.io/mta/mta-$item-external-provider-rhel9:$version"
     echo "$item image is tagged"
 done
