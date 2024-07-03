bfc7a58544b887c95012aa4743e4548a9cb34441
Igor Braginsky <ibragins@redhat.com>
2024-07-03 17:10:05 +0300

    Added main image to the list
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- mta-cli_pull_images.sh
+++ mta-cli_pull_images.sh
@@ -11,21 +11,21 @@ fi
 
 echo "Attention, using $CMD as container engine"
 echo "Pulling D/S images..."
-for item in java generic dotnet; do
+for item in java generic dotnet cli; do
     package=$(cat output.txt | grep registry | grep "$item" | cut -d "\"" -f 2|cut -d "/" -f 3)
     $CMD pull "registry-proxy.engineering.redhat.com/rh-osbs/mta-$package"
 done
 
 echo --------------------
 echo "Getting hash..."
-for item in java generic dotnet; do
+for item in java generic dotnet cli; do
     hashes[$item]=$($CMD images | grep "$item" | grep none | awk '{print $3}')
     echo "$item's hash is ${hashes[$item]}"
 done
 
 echo --------------------
 echo "Tagging images..."
-for item in java generic dotnet; do
+for item in java generic dotnet cli; do
     $CMD tag ${hashes[$item]} "registry.redhat.io/mta/mta-$item-external-provider-rhel9:$version"
     echo "$item image is tagged"
 done
