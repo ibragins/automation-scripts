915a93489cf47e870e63d20c239b33318836cc3b
Igor Braginsky <ibragins@redhat.com>
2024-07-03 22:56:38 +0300

    Added CLI image pulling for MAC
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

---
+++ mta-cli_pull_images_mac.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+version="7.1.0"
+images=("java" "generic" "dotnet" "cli")
+hashes=()
+
+if [ "$1" == "docker" ]; then
+    CMD="docker"
+else
+    CMD="podman"
+fi
+
+echo "Attention, using $CMD as container engine"
+echo "Pulling D/S images..."
+for item in "${images[@]}"; do
+    package=$(grep registry output.txt | grep "$item" | cut -d "\"" -f 2 | cut -d "/" -f 3)
+    $CMD pull "brew.registry.redhat.io/rh-osbs/mta-$package"
+done
+
+echo --------------------
+echo "Getting hash..."
+for item in "${images[@]}"; do
+    hash=$($CMD images | grep "$item" | grep none | awk '{print $3}')
+    hashes+=("$hash")
+    echo "$item's hash is $hash"
+done
+
+echo --------------------
+echo "Tagging images..."
+for i in "${!images[@]}"; do
+    item=${images[$i]}
+    hash=${hashes[$i]}
+    if [ "$item" != "cli" ]; then
+        $CMD tag "$hash" "registry.redhat.io/mta/mta-$item-external-provider-rhel9:$version"
+        echo "$item image is tagged"
+    else
+        $CMD tag "$hash" "registry.redhat.io/mta/mta-cli-rhel9:$version"
+        echo "cli image is tagged"
+    fi
+done
